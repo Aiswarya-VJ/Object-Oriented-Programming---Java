7d6daac08e9c9c70b2e94b990df90966530faf6a
Aiswarya V J <aiswaryaanandhu9@gmail.com>
2025-05-28 20:35:23 +0530

    Update Stringlength.java

--- Stringlength.java
+++ Stringlength.java
@@ -2,7 +2,7 @@ public class Stringlength {
     
 
   public static void main(String[] args) {
-    String txt = "ABCDEFJ";
+    String txt = "Java";
     System.out.println("The length of the txt string is: " + txt.length());
   
 }
